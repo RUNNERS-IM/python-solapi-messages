ca6c19d45f94d036c89291f20f0f9523cee46d1a
Ocean <ocean@runners.im>
2024-08-13 22:52:25 +0900

    수정 : setup.py 수정

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="solapi-sms",
+    name="python-solapi-messages",
     version="0.1.0",
     author="RUNNERS",
     author_email="dev@runners.im",
-    description="A Python library for sending SMS using SolApi",
+    description="A Python library for sending messages using SolApi",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/RUNNERS-IM/python-solapi-sms",
+    url="https://github.com/RUNNERS-IM/python-solapi-messages",
     packages=find_packages(exclude=["tests"]),
     classifiers=[
         "Development Status :: 3 - Alpha",
